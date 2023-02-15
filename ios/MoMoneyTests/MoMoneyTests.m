f73422f69a5872f4e77282da3c0f7a6fcd9f5c63
Austin Milt <austin.w.milt@gmail.com>
2023-02-15 16:50:39 -0500

    change app name to TapCash

--- ios/MoMoneyTests/MoMoneyTests.m
+++ ios/MoMoneyTests/MoMoneyTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MoCashTests : XCTestCase
+@interface TapCashTests : XCTestCase
 
 @end
 
-@implementation MoCashTests
+@implementation TapCashTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
