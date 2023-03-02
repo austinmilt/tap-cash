2b5ed6d967c6cc9ed8ed1dbbf9ae6877a4f7b9d1
Austin Milt <austin.w.milt@gmail.com>
2023-03-02 16:32:55 -0500

    add github action to run tests

--- backend/src/handlers/withdraw.test.ts
+++ backend/src/handlers/withdraw.test.ts
@@ -1,13 +1,3 @@
-import { MockHttpResponse } from "../dev/testing/MockHttpResponse";
-import { ApiInitializeMemberRequest, ApiSendRequest } from "../shared/api";
-import { DatabaseClient } from "../db/client";
-import { InMemoryDatabaseClient } from "../dev/testing/InMemoryDatabaseClient";
-import { setDatabaseClient, setTapCashClient } from "../helpers/singletons";
-import { handleNewMember } from "./new-member";
-import { buildPostRequest } from "../dev/testing/utils";
-import { MockTapCashClient } from "../dev/testing/MockTapCashClient";
-import { Keypair, PublicKey } from "../helpers/solana";
-import { handleSend } from "./send";
 
 
 describe('withdraw handler', () => {
