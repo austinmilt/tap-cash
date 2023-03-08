cbb21d3d3ce63083fd36f4417eda4f360f180d26
amilz.sol <85324096+amilz@users.noreply.github.com>
2023-03-08 14:48:00 -0800

    feat/add format date function

--- src/common/number.ts
+++ src/common/number.ts
@@ -9,3 +9,13 @@ const USDC_FORMATTER = Intl.NumberFormat(undefined, {
 export function formatUsd(amount: number): string {
     return USDC_FORMATTER.format(amount);
 }
+
+export function formatDate(unixTimestamp: number): string {
+    const date = new Date(unixTimestamp * 1000); // Convert to milliseconds
+    const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const monthIndex = date.getMonth();
+    const year = date.getFullYear();
+    const day = date.getDate();
+    return `${monthNames[monthIndex]} ${day}, ${year}`;
+  }
+  
