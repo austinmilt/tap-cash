a76d5efffc1df76140fc0f2a3acd8e15df2763df
amilz.sol <85324096+amilz@users.noreply.github.com>
2023-03-08 20:07:06 -0800

    add recent activity icons

--- src/components/Activity.tsx
+++ src/components/Activity.tsx
@@ -3,6 +3,7 @@ import { Text } from "../components/Text";
 import {  StyleSheet } from 'react-native';
 import { formatDate, formatUsd } from "../common/number";
 import { MemberActivity, MemberActivityType } from "../shared/activity";
+import { Image } from 'react-native-ui-lib';
 
 interface Props {
     item: MemberActivity;
@@ -12,42 +13,54 @@ export function Activity({ item }: Props): JSX.Element {
     let amount: number | undefined;
     let type: string;
     let color: string;
+    let iconSource;
 
     switch (item.type) {
         case MemberActivityType.DEPOSIT:
             amount = item.deposit?.amount;
             type = 'Deposit';
             color = 'black';
+            iconSource = require('../images/activity/deposit.png');
             break;
 
         case MemberActivityType.WITHDRAW:
             amount = item.withdraw?.amount;
             type = 'Withdraw';
             color = 'red';
+            iconSource = require('../images/activity/withdraw.png');
+
             break;
 
         case MemberActivityType.RECEIVE:
             amount = item.receive?.amount;
             type = 'Receive';
             color = 'green';
+            iconSource = require('../images/activity/recieve.png');
+
             break;
 
         case MemberActivityType.SEND:
             amount = item.send?.amount;
             type = 'Send';
             color = 'blue';
+            iconSource = require('../images/activity/send.png');
             break;
 
         default:
             amount = 0;
             color = 'black';
             type = '';
+            iconSource = null;
             break;
     }
 
     return (
         <View style={activityStyles.row}>
-            <View style={activityStyles.icon} />
+            <Image
+                style={activityStyles.icon}
+                source={iconSource}
+                resizeMode="contain"
+            />
             <View style={activityStyles.info}>
                 <Text gray-dark text-md>{type}</Text>
                 <Text gray-gray-medium text-md>
@@ -73,9 +86,7 @@ const activityStyles = StyleSheet.create({
     icon: {
         width: 48,
         height: 48,
-        borderRadius: 12,
-        backgroundColor: '#ccc',
-        marginRight: 16,
+        marginRight: 16
     },
     info: {
         flex: 1,
